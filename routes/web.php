1d351a25806a2fd84be2338a2a87102aac2d470b
三好和之 <miyoshikazuyuki@miyoshikazuyukinoMacBook-puro.local>
2020-03-28 15:09:45 +0900

    auth手前まで

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,11 @@ Route::group(['prefix' => 'admin'], function(){
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
